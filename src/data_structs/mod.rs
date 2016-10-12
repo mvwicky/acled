2124725bdc8b75bcdf164b96bcafd97e43dde45f
Michael Van Wickle <vanwickle.m@husky.neu.edu>
2016-10-12 19:10:08 -0400

    started adding db functionality

--- src/data_structs/mod.rs
+++ src/data_structs/mod.rs
@@ -24,8 +24,8 @@ pub struct Event {
     pub admin_2: String,
     pub admin_3: String,
     pub location: String,
-    pub latitude: f32,
-    pub longitude: f32,
+    pub latitude: f64,
+    pub longitude: f64,
     pub geo_precision: i32,
     pub source: String,
     pub notes: String,
@@ -34,7 +34,23 @@ pub struct Event {
 
 #[derive(Debug, Clone, RustcEncodable)]
 pub struct EventTrunc {
-
+    pub event_date: NaiveDate,
+    pub year: i32,
+    pub event_type: String,
+    pub actor_1: String,
+    pub ally_actor_1: String,
+    pub actor_2: String,
+    pub ally_actor_2: String,
+    pub country: String,
+    pub admin_1: String,
+    pub admin_2: String,
+    pub admin_3: String,
+    pub location: String,
+    pub latitude: f64,
+    pub longitude: f64,
+    pub source: String,
+    pub notes: String,
+    pub fatalities: i32,
 }
 
 
@@ -65,6 +81,15 @@ pub struct MainPageData {
     pub countries: Vec<CountryNameLink>,
 }
 
+
+// struct to contain all country objects
+// i.e., contains all data basically
+// searching for countries (by name/link) will be implemented on this
+#[derive(Clone, RustcEncodable)]
+pub struct AllCountries {
+    pub countries: Vec<Country>,
+}
+
 #[derive(Debug, Clone, RustcEncodable)]
 pub struct Year {
     pub year: i32,
@@ -74,14 +99,15 @@ pub struct Year {
     pub fpe: String,
 }
 
+
 impl Event {
     pub fn from_row(row: &Vec<String>) -> Event {
         let date_str: String = row[3].parse::<String>().unwrap();
         let t_date: NaiveDate = NaiveDate::parse_from_str(&date_str, "%d/%m/%Y").unwrap();
         let t_year: i32 = row[4].parse::<i32>().unwrap();
         let t_tpres: i32 = row[5].parse::<i32>().unwrap();
-        let t_lat: f32 = row[19].parse::<f32>().unwrap();
-        let t_lon: f32 = row[20].parse::<f32>().unwrap();
+        let t_lat: f64 = row[19].parse::<f64>().unwrap();
+        let t_lon: f64 = row[20].parse::<f64>().unwrap();
         let t_gpres: i32 = row[21].parse::<i32>().unwrap();
         let t_fat: i32 = row[24].parse::<i32>().unwrap();
         Event {
@@ -113,6 +139,58 @@ impl Event {
         }
     }
 }
+
+impl EventTrunc {
+    pub fn from_event(event: &Event) -> EventTrunc {
+        EventTrunc {
+            event_date: event.event_date.clone(),
+            year: event.year.clone(),
+            event_type: event.event_type.clone(),
+            actor_1: event.actor_1.clone(),
+            ally_actor_1: event.ally_actor_1.clone(),
+            actor_2: event.actor_2.clone(),
+            ally_actor_2: event.ally_actor_2.clone(),
+            country: event.country.clone(),
+            admin_1: event.admin_1.clone(),
+            admin_2: event.admin_2.clone(),
+            admin_3: event.admin_3.clone(),
+            location: event.location.clone(),
+            latitude: event.latitude.clone(),
+            longitude: event.longitude.clone(),
+            source: event.source.clone(),
+            notes: event.notes.clone(),
+            fatalities: event.fatalities.clone(),
+        }
+    }
+    pub fn from_row(row: &Vec<String>) -> EventTrunc {
+        let date_str: String = row[3].parse::<String>().unwrap().trim().to_string();
+        let t_date: NaiveDate = NaiveDate::parse_from_str(&date_str, "%d/%m/%Y").unwrap();
+        let t_year: i32 = row[4].parse::<i32>().unwrap();
+        let t_lat: f64 = row[19].parse::<f64>().unwrap();
+        let t_lon: f64 = row[20].parse::<f64>().unwrap();
+        let t_fat: i32 = row[24].parse::<i32>().unwrap();
+        EventTrunc {
+            event_date: t_date,
+            year: t_year,
+            event_type: row[6].clone().trim().to_string(),
+            actor_1: row[7].clone().trim().to_string(),
+            ally_actor_1: row[8].clone().trim().to_string(),
+            actor_2: row[10].clone().trim().to_string(),
+            ally_actor_2: row[11].clone().trim().to_string(),
+            country: row[14].clone().trim().to_string(),
+            admin_1: row[15].clone().trim().to_string(),
+            admin_2: row[16].clone().trim().to_string(),
+            admin_3: row[17].clone().trim().to_string(),
+            location: row[18].clone().trim().to_string(),
+            latitude: t_lat,
+            longitude: t_lon,
+            source: row[22].clone().trim().to_string(),
+            notes: row[23].clone().trim().to_string(),
+            fatalities: t_fat,
+        }
+    }
+}
+
 
 impl Country {
     pub fn new(t_link: String, t_name: String, num_eve: i32, num_fat: i32) -> Country {
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ use std::collections::HashMap;
 
 use nickel::{Nickel, HttpRouter, Mountable, StaticFilesHandler};
 
-use acled::data_structs::{Event, Country, CountryPageData, MainPageData, CountryNameLink};
+use acled::data_structs::{Event, EventTrunc, Country, CountryPageData, MainPageData,
+                          CountryNameLink};
 
 use postgres::{Connection, TlsMode};
 
@@ -59,8 +60,6 @@ fn get_country_by_link(inp_vec: &Vec<Country>, inp_link: String) -> Option<Count
 }
 
 fn main() {
-    let conn = Connection::connect("postgresql://michael@localhost/acled", TlsMode::None).unwrap();
-
     let mut p = env::current_dir().unwrap();
     p.push("data");
     p.push("csv");
@@ -71,16 +70,42 @@ fn main() {
         println!("File does not exist! {}", p.display());
         std::process::exit(-1);
     }
+    let conn = Connection::connect("postgresql://michael@localhost/acled", TlsMode::None).unwrap();
+
 
     conn.execute("DROP TABLE IF EXISTS event", &[]).unwrap();
+    // conn.execute("DROP TABLE IF EXISTS event_trunc", &[]).unwrap();
 
+    conn.execute("CREATE TABLE IF NOT EXISTS event_trunc (\
+                    id           SERIAL PRIMARY KEY,\
+                    event_date   DATE,\
+                    year         INT,\
+                    event_type   VARCHAR NOT NULL,\
+                    actor_1      VARCHAR NOT NULL,\
+                    ally_actor_1 VARCHAR NOT NULL,\
+                    actor_2      VARCHAR NOT NULL,\
+                    ally_actor_2 VARCHAR NOT NULL,\
+                    country      VARCHAR NOT NULL,\
+                    admin_1      VARCHAR NOT NULL,\
+                    admin_2      VARCHAR NOT NULL,\
+                    admin_3      VARCHAR NOT NULL,\
+                    location     VARCHAR NOT NULL,\
+                    latitude     DOUBLE PRECISION,\
+                    longitude    DOUBLE PRECISION,\
+                    source       VARCHAR NOT NULL,\
+                    notes        VARCHAR NOT NULL,\
+                    fatalities   INT\
+                  )",
+                 &[])
+        .unwrap();
+
+    let mut cur_rows: i64 = 0;
+    for cnt in &conn.query("SELECT count(*) from event_trunc", &[]).unwrap() {
+        cur_rows = cnt.get(0);
+    }
 
     let mut rdr = csv::Reader::from_file(p.as_path()).unwrap();
 
-    println!("HEADERS");
-    for head in rdr.headers().unwrap() {
-        println!("{:?}", head);
-    }
     println!("");
 
 
@@ -91,9 +116,45 @@ fn main() {
     let mut country_vec: Vec<Country> = Vec::new();
     let mut tot_eve: i32 = 0;
     let mut last_country: Option<String> = None;
+
+    let add_event_trunc =
+        conn.prepare("INSERT INTO event_trunc (event_date, year, event_type, \
+                      actor_1,ally_actor_1, actor_2, ally_actor_2, country,admin_1, admin_2, \
+                      admin_3, location,latitude, longitude, source, notes,fatalities) VALUES \
+                      ($1, $2, $3, $4,$5, $6, $7, $8,$9, $10, $11, $12,$13, $14, $15, $16,$17)")
+            .unwrap();
+
+    let get_event_trunc_country = 
+        conn.prepare("SELECT * FROM event_trunc WHERE country = $1").unwrap();
+
+    let mut rows_aff = 0;
     for row in rdr.records() {
         tot_eve += 1;
         let row = row.unwrap();
+
+        let e_t = EventTrunc::from_row(&row);
+        if cur_rows == 0 {
+            let r = add_event_trunc.execute(&[&e_t.event_date,
+                           &e_t.year,
+                           &e_t.event_type,
+                           &e_t.actor_1,
+                           &e_t.ally_actor_1,
+                           &e_t.actor_2,
+                           &e_t.ally_actor_2,
+                           &e_t.country,
+                           &e_t.admin_1,
+                           &e_t.admin_2,
+                           &e_t.admin_3,
+                           &e_t.location,
+                           &e_t.latitude,
+                           &e_t.longitude,
+                           &e_t.source,
+                           &e_t.notes,
+                           &e_t.fatalities])
+                .unwrap();
+            rows_aff += r;
+            println!("{}", rows_aff);
+        }
 
         let fatalities = row[fatality_index].parse::<i32>().unwrap();
         let country = row[country_index].parse::<String>().unwrap();
@@ -122,6 +183,7 @@ fn main() {
         country_vec[ind].num_fatalities += fatalities;
         country_vec[ind].events.push(Event::from_row(&row));
     }
+    println!("Rows Affected: {}", rows_aff);
     println!("Total Events: {}", tot_eve);
 
     let mut server = Nickel::new();
@@ -132,9 +194,6 @@ fn main() {
     server.mount("/js/", StaticFilesHandler::new("src/js"));
 
     let c_vec = country_vec.clone();
-    let test_ctry_page_data: CountryPageData = c_vec[0].to_page_data();
-    println!("{}", test_ctry_page_data.years.len());
-
     // Main Page
     let main_page = MainPageData { countries: country_nl_vec };
     server.get("/",
